08760fa7cf0212a1e92b47a7294d785396d7e3fd
hscastro <halisonsc5@gmail.com>
2020-12-17 22:49:43 -0300

    Implementação da aplicação cliente da api

---
+++ creditoSave.php
@@ -0,0 +1,26 @@
+<?php
+	//Desenvolvedor: Antonio Halyson 
+	//email: halisonsc5@gmail.com
+	//Data:16/12/2020 as 21:10
+    //http://github.com/hscastro
+    
+    require_once('Conta.class.php');
+    require_once('ContaService.class.php');
+
+    $numero = $_GET['numero'];
+    $valor = $_GET['valor'];
+        
+    
+    $tester = new ContaService;
+
+    $conta = new Conta;
+    
+    $conta->setNumero($numero);
+    
+    //salva no banco de daos
+    $res = $tester->setCreditoConta($conta, $valor);
+
+    echo $res;
+    //print_r(json_encode($res));
+
+?>
